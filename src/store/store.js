f69eaf25bae1b96f50cd1af72949fa31516d2379
hamidrzzaa <hamidrzaa11@gmail.com>
2019-12-25 15:27:48 +0330

    seed data have been removed

--- src/store/store.js
+++ src/store/store.js
@@ -9,29 +9,4 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-store.dispatch(
-  addExpense({
-    description: "money for the rent",
-    note: "here we go",
-    amount: 400,
-    createdAt: Date.now()
-  })
-);
-store.dispatch(
-  addExpense({
-    description: "money for BMW m4",
-    note: "here we go",
-    amount: 50000,
-    createdAt: Date.now()
-  })
-);
-store.dispatch(
-  addExpense({
-    description: "money for the razor blade",
-    note: "here we go",
-    amount: 1400,
-    createdAt: Date.now()
-  })
-);
-
 export default store;
