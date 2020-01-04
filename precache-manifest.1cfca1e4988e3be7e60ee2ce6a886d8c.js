5eb1f03bfa6aba608f8bb9f974dfcbdb2ed1688a
hamidrzzaa <hamidrzaa11@gmail.com>
2020-01-04 14:05:12 +0330

    Updates

---
+++ precache-manifest.1cfca1e4988e3be7e60ee2ce6a886d8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea485f79949b6890ab97f2e2adcb6c5",
+    "url": "/monster-roldex/index.html"
+  },
+  {
+    "revision": "53cf5fcec9c86ffb7672",
+    "url": "/monster-roldex/static/css/main.6fa31075.chunk.css"
+  },
+  {
+    "revision": "a0f5f11f92b40528aa65",
+    "url": "/monster-roldex/static/js/2.eb9a484b.chunk.js"
+  },
+  {
+    "revision": "53cf5fcec9c86ffb7672",
+    "url": "/monster-roldex/static/js/main.d3d2249c.chunk.js"
+  },
+  {
+    "revision": "db17e2dd6d61c588f02d",
+    "url": "/monster-roldex/static/js/runtime-main.d986b7eb.js"
+  }
+]);
